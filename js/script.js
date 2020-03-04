d8ff4490569766ce92f33bacdd1be6d516c3257d
urmilshroff <urmilshroff@gmail.com>
2020-03-04 23:42:10 +0530

    Done

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-var deathTime = new Date("April 17, 2020 17:00:00").getTime();
+var end = new Date("April 17, 2020 17:00:00").getTime();
 
-var bTechIntegrated = setInterval(function() {
+var countdown = setInterval(function() {
     var now = new Date().getTime();
-    var distance = deathTime - now;
+    var distance = end - now;
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById("bti-countdown").innerHTML = "BTI over in " + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
     if (distance < 0) {
-        clearInterval(bTechIntegrated);
+        clearInterval(countdown);
     }
 
 }, 1000);
